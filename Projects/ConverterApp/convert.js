4ddba7041e74ecdb84246d0f99d074910aea292e
Tanveer Alam <heytanveeralam@gmail.com>
2019-01-30 17:03:55 -0500

    Update convert.js

--- Projects/ConverterApp/convert.js
+++ Projects/ConverterApp/convert.js
@@ -4,8 +4,8 @@ document.getElementById('dlinput').addEventListener('input' ,
 			function(e) {
 				
 				let dollar = e.target.value;
-				let total = dollar*83.73;
-				document.getElementById('takaOutput').innerHTML = total.toFixed(2);
+				let total = dollar*83.73.toFixed();
+				document.getElementById('takaOutput').innerHTML = total);
 
 		}
 		);
