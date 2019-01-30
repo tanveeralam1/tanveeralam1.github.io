feb3ae1c667af6664054cdbbca61149626707119
Tanveer Alam <heytanveeralam@gmail.com>
2019-01-30 16:48:55 -0500

    Delete convert.js

--- convert.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('dlinput').addEventListener('input' , 
-			function(e) {
-				
-				let dollar = e.target.value;
-				document.getElementById('takaOutput').innerHTML = dollar*83.73;
-
-		}
-		);
