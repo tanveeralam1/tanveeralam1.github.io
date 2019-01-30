3da841cb56b2683e72ada9380314bcb3af5df4fa
Tanveer Alam <heytanveeralam@gmail.com>
2019-01-30 16:58:42 -0500

    Update convert.js

--- Projects/ConverterApp/convert.js
+++ Projects/ConverterApp/convert.js
@@ -1,8 +1,10 @@
+
+//DOM Manipulation to Listen for Input, and then Outputs the converted currency value
 document.getElementById('dlinput').addEventListener('input' , 
 			function(e) {
 				
 				let dollar = e.target.value;
-				document.getElementById('takaOutput').innerHTML = dollar*83.73;
+				document.getElementById('takaOutput').innerHTML = dollar*83.73.toFixed(2);
 
 		}
 		);
